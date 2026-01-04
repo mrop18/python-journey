e8a50e17927caf7a5145c1495ea229c4546f7e5a
Om Prakash Kumawat <mrop18@gmail.com>
2026-01-04 09:42:26 +0530

    changed hello file

--- day00/hello.py
+++ day00/hello.py
@@ -1,1 +1,10 @@
 print("Hello, Let's start Python")
+
+# to comment use (#)
+# this is first comment line
+spam = 1 # this is secont comment line
+#... and now a third
+text = "# this is not a comment, becuase it's inside quotes."
+
+print(spam)
+print(text)

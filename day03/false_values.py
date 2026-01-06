1663e1bf8917c136124014e41e344e7e531a4d5e
Om Prakash Kumawat <mrop18@gmail.com>
2026-01-06 21:29:30 +0530

    added false_values

---
+++ day03/false_values.py
@@ -0,0 +1,17 @@
+# False Values
+    # False
+    # None
+    # Zero of any numeric type
+    # Any Empty sequence, for example, '', (), [],
+    # Any Empty mapping, for example, {},
+
+# condition = False
+# condition = None
+# condition = 0
+condition = ()
+
+
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluated to False')

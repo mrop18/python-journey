5d980d724ced08a899c3c30e4e30527261bfc5aa
Om Prakash Kumawat <mrop18@gmail.com>
2026-01-07 20:08:16 +0530

    added loops

---
+++ day04/loops.py
@@ -0,0 +1,30 @@
+# nums = [1, 2, 3, 4, 5]
+
+# for num in nums:
+#     print(num)
+
+# for num in nums:
+#     if num == 3:
+#         print('found')
+#         # break
+#         continue
+#     print(num)
+
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter)
+
+
+# for i in range(10):
+# for i in range(1, 11):
+#     print(i)
+
+
+x = 0
+
+# while x<10:
+while True:
+    if x == 5:
+        break
+    print(x)
+    x += 1
